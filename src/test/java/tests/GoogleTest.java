6355c193762039c350861264d131bee96c654a37
jeevan <johny60thomas@gmail.com>
2025-07-08 22:50:04 +0530

    +

---
+++ src/test/java/tests/GoogleTest.java
@@ -0,0 +1,17 @@
+package tests;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class GoogleTest {
+    public static void main(String[] args) {
+        WebDriverManager.chromedriver().setup(); // sets up chromedriver automatically
+        WebDriver driver = new ChromeDriver();
+
+        driver.get("https://www.google.com");
+        System.out.println("Title is: " + driver.getTitle());
+
+        driver.quit();
+    }
+}
