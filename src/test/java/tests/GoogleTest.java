496430f77546e9eef599754cb96e82821adc0cf1
jeevan <johny60thomas@gmail.com>
2025-07-09 23:19:06 +0530

    Added step for window maximizing

--- src/test/java/tests/GoogleTest.java
+++ src/test/java/tests/GoogleTest.java
@@ -8,7 +8,7 @@ public class GoogleTest {
     public static void main(String[] args) {
         WebDriverManager.chromedriver().setup(); // sets up chromedriver automatically
         WebDriver driver = new ChromeDriver();
-
+        driver.manage().window().maximize();
         driver.get("https://www.google.com");
         System.out.println("Title is: " + driver.getTitle());
 
